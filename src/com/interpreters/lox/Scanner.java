45dc0ec32fe88b302f3d8285c45f7b0b5f7c8bf1
Andy Filchev <andy.filchev@protonmail.com>
2024-03-19 17:17:19 +0200

    feat: add default case for lexicical errors

--- src/com/interpreters/lox/Scanner.java
+++ src/com/interpreters/lox/Scanner.java
@@ -46,6 +46,10 @@ public class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
+
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
         }
     }
 
