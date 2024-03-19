98b8132183f01a0be719dc736f8ca435a6dcbe0e
Andy Filchev <andy.filchev@protonmail.com>
2024-03-19 12:40:00 +0200

    feat: add methods in Scanner to handle single character tokens

--- src/com/interpreters/lox/Scanner.java
+++ src/com/interpreters/lox/Scanner.java
@@ -32,8 +32,42 @@ public class Scanner {
         return tokens;
     }
 
+    /* Scan for tokens */
+    private void scanToken() {
+        char c = advance();
+        switch(c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+        }
+    }
+
     /* Check if the end of the line is reached */
     private boolean isAtEnd() {
         return current >= source.length();
     }
+
+    /* InputAction: Take the next char from the source and return it */
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    /* OutputAction: Shortcut to add token to the token list */
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    /* OutputAction: Add token with a literal value to the token list */
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
 }
