5effc8432dcc9cf3a76eea1c3a725eb875fc92c7
Andy Filchev <andy.filchev@protonmail.com>
2024-03-28 14:45:23 +0200

    feat: add AST printer

---
+++ src/com/interpreters/lox/AstPrinter.java
@@ -0,0 +1,67 @@
+package com.interpreters.lox;
+
+class AstPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    /* Convert the literal to string or 'nil' */
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    /**
+     * Helper method to handle subexpressions. It takes a:
+     * @param   name
+     * @param   exprs   list of subexpressions
+     * @return          parenthesized string with subexpressions
+     */
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(").append(name);
+        for (Expr expr : exprs) {
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        // Instantiate a tree
+        Expr expression = new Expr.Binary(
+                // left
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+
+                // oprerator
+                new Token(TokenType.STAR, "*", null, 1),
+
+                // right
+                new Expr.Grouping(new Expr.Literal(45.67))
+        );
+
+        // Print the tree
+        System.out.println(new AstPrinter().print(expression));
+    }
+}
