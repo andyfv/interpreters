5c161797dccb4082e0470e0fd990f309e26caebe
Andy Filchev <andy.filchev@protonmail.com>
2024-04-03 23:41:58 +0300

    feat: add evaluation binary arithmetic operators

--- src/com/interpreters/lox/Interpreter.java
+++ src/com/interpreters/lox/Interpreter.java
@@ -32,6 +32,27 @@ class Interpreter implements Expr.Visitor<Object> {
 
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left     = evaluate(expr.left);
+        Object right    = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            // Arithmetic
+            case MINUS: return (double)left - (double)right;
+            case SLASH: return (double)left - (double)right;
+            case STAR:  return (double)left * (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+                break;
+
+            //
+        }
+
         return null;
     }
 
