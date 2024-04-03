d9942a8dcbda8bb7fdb9abcee35244dbd1968135
Andy Filchev <andy.filchev@protonmail.com>
2024-04-03 23:54:15 +0300

    feat: add evaluation for equality operators

--- src/com/interpreters/lox/Interpreter.java
+++ src/com/interpreters/lox/Interpreter.java
@@ -36,6 +36,10 @@ class Interpreter implements Expr.Visitor<Object> {
         Object right    = evaluate(expr.right);
 
         switch (expr.operator.type) {
+            // Equality
+            case BANG_EQUAL:    return !isEqual(left, right);
+            case EQUAL_EQUAL:   return isEqual(left, right);
+
             // Comparison
             case GREATER:       return (double)left > (double)right;
             case GREATER_EQUAL: return (double)left >= (double)right;
@@ -43,9 +47,9 @@ class Interpreter implements Expr.Visitor<Object> {
             case LESS_EQUAL:    return (double)left <= (double)right;
 
             // Arithmetic
-            case MINUS: return (double)left - (double)right;
-            case SLASH: return (double)left - (double)right;
-            case STAR:  return (double)left * (double)right;
+            case MINUS:         return (double)left - (double)right;
+            case SLASH:         return (double)left - (double)right;
+            case STAR:          return (double)left * (double)right;
             case PLUS:
                 if (left instanceof Double && right instanceof Double) {
                     return (double)left + (double)right;
@@ -58,6 +62,17 @@ class Interpreter implements Expr.Visitor<Object> {
         }
 
         return null;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+
+        /* Handle the situation if @a is null because we don't want to
+        invoke equals() on a a null
+        */
+        if (a == null) return false;
+
+        return a.equals(b);
     }
 
     private Object evaluate(Expr expr) {
