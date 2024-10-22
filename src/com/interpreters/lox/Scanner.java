cfe423c97da76fad12ee251e3981f945d10c9d4b
Andy Filchev <andy.filchev@protonmail.com>
2024-10-23 00:15:42 +0300

    style: reformat code

--- src/com/interpreters/lox/Scanner.java
+++ src/com/interpreters/lox/Scanner.java
@@ -85,7 +85,7 @@ public class Scanner {
                 if (match('/')) { // A comment goes until the end of the line.
                     while (peek() != '\n' && !isAtEnd()) advance();
                 } else {
-                addToken(SLASH);
+                    addToken(SLASH);
                 }
                 break;
 
